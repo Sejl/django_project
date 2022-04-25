8eecec1e51c22b8f822aa0f8dfe45ef2d3887fc5
sejl <sejlau@gmail.com>
2022-04-25 12:45:45 +0200

    Test .gitignore

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'author': 'Sejl',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2022'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2022'
+    }
+
+]
+
+
 # Create your views here.
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html')
+
