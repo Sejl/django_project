123088b1d8fe1e45f6ab8712399d86d01da2ef8a
sejl <sejlau@gmail.com>
2022-04-26 08:10:39 +0200

    Auth system

--- django_project/settings.py
+++ django_project/settings.py
@@ -125,4 +125,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# My config:
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL = 'login'
