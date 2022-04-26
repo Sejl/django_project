a2b938e974a10e4ee7e26920b1f509f9a97a8601
sejl <sejlau@gmail.com>
2022-04-26 11:37:13 +0200

    Profile update, pictures upload, resize

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 # Create your models here.
 class Profile(models.Model):
@@ -9,7 +10,17 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    
+    # override save method to save smaller img file
+    def save(self):
+        super().save()   # run save method of the parent class, save image
+
+        img = Image.open(self.image.path) # open image
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path) # save over the same image
+
 
 
 
